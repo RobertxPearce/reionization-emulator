2a6a9e3e1931bdacc11e049f52984abc73d5045e
RobertxPearce <robertbdpearce@gmail.com>
2025-10-29 09:25:02 -0700

    Update run simulation script

---
+++ scripts/build_dataset.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------------------
+# Build data set for emulator. Extract kSZ Maps and reionization parameters into HDF5
+# Robert Pearce
+#-------------------------------------------------------------------------------------------
+
+import argparse
+from pathlib import Path
+import re
+import sys
+from typing import Optional
+import h5py
+import numpy as np
+
+
+
+# ksz map
+# pt_tt
+# xmval_list
+# zval_list
+# alpha_zre
+# kb_zre
+# zmean_zre
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
+
+#-----------------------------
+#         END OF FILE
+#-----------------------------
